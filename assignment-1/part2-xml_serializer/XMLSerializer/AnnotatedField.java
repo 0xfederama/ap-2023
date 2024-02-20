a376edc601e7c12a78e5a6fcb1b2472502359ff6
Federico <vgfede@gmail.com>
2024-02-20 15:27:30 +0100

    commented annotated field

--- assignment-1/part2-xml_serializer/XMLSerializer/AnnotatedField.java
+++ assignment-1/part2-xml_serializer/XMLSerializer/AnnotatedField.java
@@ -2,6 +2,10 @@ package XMLSerializer;
 
 import java.lang.reflect.Field;
 
+/**
+ * This class is used to store the introspected classes, storing the fields
+ * together with their name and type
+ */
 public class AnnotatedField {
 	private final Field field;
 	private final String name;
